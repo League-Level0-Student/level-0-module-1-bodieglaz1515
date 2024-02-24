088fc0a3c154e658cf5931abfb0d64198bb00f6a
league <league@192.168.1.200>
2024-02-23 16:56:14 -0800

    working on remakable

---
+++ src/_03_if_else/_1_unbirthday/Meow.java
@@ -0,0 +1,40 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Meow {
+public static void main(String[] args) {
+	String input1= JOptionPane.showInputDialog("what is your birthday");
+	if( input1.equals ("3/23")){
+		JOptionPane.showMessageDialog(null, "happy birthday");
+		
+	}else {
+		
+		
+		
+		
+		JOptionPane.showMessageDialog(null, "happy UNBIRTHDAY");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -5,13 +5,30 @@ package _03_if_else._2_remarkable;
 import javax.swing.JOptionPane;
 
 public class Remarkable {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
+		String locke = "your favorite food is banana";jj
 
+		String input = JOptionPane.showInputDialog("what is your name");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
+		if (input.equalsIgnoreCase("locke")) {
+
+			JOptionPane.showMessageDialog(null, locke);
+
+		}else if(input.equalsIgnoreCase("william")) {
+
+
+			JOptionPane.showMessageDialog(null, william);
+
+		}else {
+			
+			JOptionPane.showMessageDialog(null,"defsou gybks please try again");
+			
+		}
+
 
 	}
 }
