cf9ac0b7c5d20aa7d73de2b2c787ce96c8c3d7bd
league <league@localhost>
2024-03-01 16:59:19 -0800

    working on badger

---
+++ src/_05_for_loops/_2_badgers/Badger.java
@@ -0,0 +1,74 @@
+package _05_for_loops._2_badgers;
+
+public class Badger {
+public static void main(String[] args) {
+	
+	
+	for (int badger=0; badger< 12; badger+=1) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.print("badger");
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
