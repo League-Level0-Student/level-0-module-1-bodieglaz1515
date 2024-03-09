e731c2f95c07f0c2dc1baedb24d218f848dc4981
league <league@192.168.1.78>
2024-03-08 16:58:33 -0800

    almost done

--- src/_05_for_loops/_2_badgers/Badger.java
+++ src/_05_for_loops/_2_badgers/Badger.java
@@ -1,74 +1,68 @@
 package _05_for_loops._2_badgers;
 
 public class Badger {
-public static void main(String[] args) {
-	
-	
-	for (int badger=0; badger< 12; badger+=1) {
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		System.out.print("badger");
-		
-		
-		
-		
-		
-		
-		
-		
+	public static void main(String[] args) {
+
+
+		meow();
+		woof(false);
+		meow();
+		woof(true);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+
+	public static void meow(){
+		for (int badger=0; badger< 12; badger+=1) {
+			System.out.print("Badger");
+
+			if (badger<11) {
+
+				System.out.print(", ");
+
+
+
+			} 
+
+		}
+		System.out.println();
+	}
+
+
+	public static void woof(boolean mushroom ) {
+
+		for (int mush=0; mush< 2; mush+=1) {
+
+			System.out.print("Mushroom");
+
+			if (mush<1) {
+
+				System.out.print(", ");
+
+
+			}else {
+				System.out.println();
+				System.out.println();
+			}
+		
+		}
+		if (mushroom) {
+		
+			
+			System.out.println();
+			System.out.println();
+			System.out.print("A snake!");
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		}
+
+
+	}
+
---
+++ src/_05_for_loops/_3_bananas_4eva/bananas_4eva/bananas_4eva.pde
@@ -0,0 +1,9 @@
+size (1000, 1000);
+int y = 10;
+
+
+for(y=10; y <1000; y +=10){
+
+for (int meow = 0; meow<20; meow +=1) {
+  text("banana", meow*50, y);
+}}
